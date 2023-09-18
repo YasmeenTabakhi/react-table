23f60285cedb0c8da23df7ee5ab9fcc17b9bd8e6
YasmeenTbakhi <yasmeenissam02@gmail.com>
2023-09-18 09:23:50 +0300

    react table features

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import {BasicTable} from './components/BasicTable'
+import {SortingTable} from './components/SortingTable'
+import {FilteringTable} from './components/FiltertingTable'
+import {StickyTable} from './components/StickyTable'
+import {PaginationTable} from './components/PaginationTable'
+function App() {
+  return (
+    <div className="App">
+      <PaginationTable />
+      
+    </div>
+  );
+}
+
+export default App;
